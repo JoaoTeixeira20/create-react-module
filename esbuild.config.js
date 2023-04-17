c6222e10f3a8fdd1de30b1ff1dc0505fb290adf8
JoaoTeixeira20 <joao.tex@msn.com>
2023-04-17 02:17:07 +0100

    testing building package with esbuild

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,6 +2,8 @@ require('esbuild').build({
     entryPoints: ['src/index.ts'],
     bundle: true,
     minify: true,
-    format: 'esm',
-    outfile: 'dist/esm/index.js',
+    format: 'cjs',
+    target: ['es6'],
+    outfile: 'dist/index.js',
+    external: ['@react-spring/web','react','react-dom']
 });
