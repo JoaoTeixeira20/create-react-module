e81edf235161bef41b1cba45e3b63e71a7c52db7
joao.teixeira <joao.teixeira@bolttech.io>
2023-04-21 22:07:47 +0100

    Adding error stack trace and prettier format

--- esbuildwatch.config.js
+++ esbuildwatch.config.js
@@ -3,32 +3,39 @@ const esbuild = require('esbuild');
 
 const esbuildPort = parseInt(process.env.ESBUILD_SERVER_PORT) || 3000;
 
-const plugins = [{
+const plugins = [
+  {
     name: 'reload',
     setup(build) {
-        build.onEnd(() => {
-            console.log(`refreshed content`);
-        })
-    }
-}];
+      build.onEnd(() => {
+        console.log(`refreshed content`);
+      });
+    },
+  },
+];
 
 async function watch() {
-    const ctx = await esbuild.context({
-        entryPoints: ['src/Preview.tsx'],
-        bundle: true,
-        minify: true,
-        format: 'esm',
-        sourcemap: true,
-        outdir: 'public/.js',
-        plugins
-    })
-    await ctx.watch();
+  const ctx = await esbuild.context({
+    entryPoints: ['src/development/Preview.tsx'],
+    bundle: true,
+    minify: true,
+    format: 'esm',
+    sourcemap: true,
+    outdir: 'public/.js',
+    define: {
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    },
+    plugins,
+  });
+  await ctx.watch();
 
-    ctx.serve({
-        servedir: 'public',
-        port: esbuildPort,
-    }).then((server) => {
-        console.log(`server started at http://localhost:${server.port}`);
+  ctx
+    .serve({
+      servedir: 'public',
+      port: esbuildPort,
+    })
+    .then((server) => {
+      console.log(`server started at http://localhost:${server.port}`);
     });
 }
 
